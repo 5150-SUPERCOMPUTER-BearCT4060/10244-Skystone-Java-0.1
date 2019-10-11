14687a2b54fa6c42f140e08f72e30c7d07215ae8
5150_SUPERCOMPUTER <5150_supercomputer_bearct4060@protonmail.com>
2019-10-10 17:01:07 -0700

    Added elbow motor, made hand with two servos, and finally put controls on trigger aswell as on the bumper instead of the colored buttons.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainDriveTeleOp_NoCoasting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainDriveTeleOp_NoCoasting.java
@@ -61,20 +61,22 @@ public class MainDriveTeleOp_NoCoasting extends OpMode {
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor left_mtr = null;
     private DcMotor right_mtr = null;
+    private DcMotor elbow_mtr = null;
     private DcMotor arm_mtr = null;
 
 
     //Servos
     //   private Servo colorArm = null;
-    private Servo arm_servo = null;
+    private Servo lHand_servo = null;
+    private Servo rHand_servo = null;
 
-    private Double LeftValue;
+    private Double LeftValue ;
     private Double RightValue;
 
-  //  private float leftPos = left_mtr.getCurrentPosition();
-//    private float rightPos = right_mtr.getCurrentPosition();
+   // private float leftPos = left_mtr.getCurrentPosition();
+   // private float rightPos = right_mtr.getCurrentPosition();
+   // private float armPos = elbow_mtr.getCurrentPosition();
    // private float armPos = arm_mtr.getCurrentPosition();
-
 
     public void init() {
         telemetry.addData("Status", "Initialized");
@@ -88,26 +90,33 @@ public class MainDriveTeleOp_NoCoasting extends OpMode {
         left_mtr = hardwareMap.get(DcMotor.class, "left_mtr");
         right_mtr = hardwareMap.get(DcMotor.class, "right_mtr");
         arm_mtr = hardwareMap.get(DcMotor.class, "arm_mtr");
+        elbow_mtr = hardwareMap.get(DcMotor.class, "elbow_mtr");
+
         //Servos
-        arm_servo = hardwareMap.get(Servo.class, "arm_servo");
+        rHand_servo = hardwareMap.get(Servo.class, "rHand_servo");
+        lHand_servo = hardwareMap.get(Servo.class, "lHand_servo");
 
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
         left_mtr.setDirection(DcMotor.Direction.FORWARD);
         right_mtr.setDirection(DcMotor.Direction.REVERSE);
         arm_mtr.setDirection(DcMotor.Direction.FORWARD);
+        elbow_mtr.setDirection(DcMotor.Direction.FORWARD);
 
         left_mtr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         right_mtr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         arm_mtr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        elbow_mtr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         left_mtr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         right_mtr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         arm_mtr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        elbow_mtr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         left_mtr.setPower(0);
         right_mtr.setPower(0);
         arm_mtr.setPower(0);
+        elbow_mtr.setPower(0);
 
 
         // Wait for the game to start (driver presses PLAY)
@@ -136,12 +145,25 @@ public class MainDriveTeleOp_NoCoasting extends OpMode {
         else if (!gamepad2.dpad_down || !gamepad2.dpad_up) {
             //liftMotor.setPower(0);
         }
-        if (gamepad2.left_bumper) {
+
+        // arm is trigger use numbers
+        if (gamepad2.left_trigger == 1) {
             arm_mtr.setPower(-0.5);
-        } else if (gamepad2.right_bumper) {
+        } else if (gamepad2.right_trigger <9) {
             arm_mtr.setPower(1);
-        } else if (!gamepad2.left_bumper || !gamepad2.right_bumper) {
+        } else if (gamepad2.left_trigger <-1 || gamepad2.right_trigger <-1) {
             arm_mtr.setPower(0.0);
+        }
+
+
+
+    //elbow is bumpers use !
+        if (gamepad2.left_bumper) {
+            elbow_mtr.setPower(-0.5);
+        } else if (gamepad2.right_bumper) {
+            elbow_mtr.setPower(1);
+        } else if (!gamepad2.left_bumper && !gamepad2.right_bumper) {
+            elbow_mtr.setPower(0.0);
         }
 
 
@@ -155,11 +177,21 @@ public class MainDriveTeleOp_NoCoasting extends OpMode {
             //clawHand.setPosition(0);
 
         } else if (gamepad2.y) {
-            //clawHand.setPosition(1.0);
+            //lHand.setPosition(1.0);
         } else if (gamepad2.a) {
-            arm_servo.setPosition(0);
+            lHand_servo.setPosition(0);
         } else if (gamepad2.b) {
-            arm_servo.setPosition(1);
+            lHand_servo.setPosition(1);
+        }
+
+
+
+         if (gamepad2.y) {
+        //rHand.setPosition(1.0);
+        } else if (gamepad2.a) {
+        rHand_servo.setPosition(0);
+        } else if (gamepad2.b) {
+        rHand_servo.setPosition(1);
         }
 
         // Send calculated power to wheels
@@ -170,10 +202,10 @@ public class MainDriveTeleOp_NoCoasting extends OpMode {
         // Show the elapsed game time and wheel power.
         telemetry.addData("Status", "Run Time: " + runtime.toString());
         telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
-      //  telemetry.addData("Left POS", "(%.2f)", leftPos);
+       //  telemetry.addData("Left POS", "(%.2f)", leftPos);
        // telemetry.addData("Right POS", "(%.2f)", rightPos);
        // telemetry.addData("Arm  POS", "(%.2f)", armPos);
-//heloo
+       // telemetry.addData("elbow  POS", "(%.2f)", armPos);
         telemetry.update();
     }
 
@@ -181,6 +213,6 @@ public class MainDriveTeleOp_NoCoasting extends OpMode {
             left_mtr.setPower(0);
             right_mtr.setPower(0);
             arm_mtr.setPower(0);
-            arm_servo.setPosition(0.5);
+            elbow_mtr.setPower(0);
             }
 }
